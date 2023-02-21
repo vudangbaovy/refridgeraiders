e3de4d8a36a7ff26a8ea7a501cb956da34a648c4
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2023-02-21 15:38:29 -0500

    integrate Angular Material for home page

--- refridgeraiders/src/app/app.module.ts
+++ refridgeraiders/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,16 +9,18 @@ import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldControl } from '@angular/material/form-field';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { SearchBarComponent } from './component/search-bar/search-bar.component';
 import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    SearchBarComponent,
+    //HomePageComponent,
+    //SearchBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +33,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatChipsModule,
     MatFormFieldModule,
     MatToolbarModule,
+    MatCardModule
   ],
   providers: [
     DataService
