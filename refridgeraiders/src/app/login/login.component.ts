882a8f8d0dc96a941513d4028115f67d7cf8ff42
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2023-03-01 14:09:25 -0700

    Connecting front to back

--- refridgeraiders/src/app/login/login.component.ts
+++ refridgeraiders/src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  loginUserData = {
+    name: "",
+    password: "",
+    adminlevel: 0,
+    allergies: " "
+  }
+  constructor(private dataService: DataService) { }
+  ngOnInit() {
+  }
+  loginUser() {
+    console.log(this.loginUserData);
+    this.dataService.loginUser(this.loginUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+  }
 }
---
+++ refridgeraiders/src/app/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    name: string;
+    password: string;
+    adminlevel: number;
+    allergies: string;
+}
