75edcfdf0723ddfd455e347909a94b430f9dc99e
mariacemo <101619695+mariacemo@users.noreply.github.com>
2023-04-19 10:11:54 -0400

    updating API documentation

--- Backend/passwords.go
+++ Backend/passwords.go
@@ -6,12 +6,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func getPass() {
-	// get password from user
-	// hashed password will be stored in the database
-
-}
-
 func hashedPass(password string) (string, error) {
 	// generates a new hashed password
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 15)
