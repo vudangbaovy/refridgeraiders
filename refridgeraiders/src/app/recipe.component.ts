4cc582a544665ab6bf4f5897333f65f20de3f0bd
duniahamed <93282302+duniahamed@users.noreply.github.com>
2023-04-19 16:52:03 -0400

    Update recipe.component.ts

--- refridgeraiders/src/app/recipe.component.ts
+++ refridgeraiders/src/app/recipe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../home/api.service';
+import { ApiService } from './home/api.service';
 import { RecipeService } from './recipe.service';
 import { Router } from '@angular/router';
 
